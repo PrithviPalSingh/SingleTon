12b69bda3153281f90630db284efda8da771fca4
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2019-07-25 15:34:22 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingelTon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SingleTonClass.Instance.GetHashCode());
+            SingleTonClass instance1 = SingleTonClass.Instance;
+            Console.WriteLine(instance1.GetHashCode());
+
+            //serialize
+            using (Stream stream = File.Open("SerializedFile.txt", FileMode.Create))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                bformatter.Serialize(stream, instance1);
+            }
+
+            using (Stream stream = File.Open("SerializedFile.txt", FileMode.Open))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                SingleTonClass instance2 = (SingleTonClass)bformatter.Deserialize(stream);
+                Console.WriteLine(instance2.GetHashCode());
+            }
+                       
+            Console.Read();
+        }
+    }
+}
---
+++ SingleTonClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingelTon
+{
+    [Serializable]
+    class SingleTonClass :ISerializable
+    {
+        private static SingleTonClass _instance;
+
+        public string Name = "SingleTon";
+
+        internal static SingleTonClass Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new SingleTonClass();
+
+                return _instance;
+            }            
+        }
+
+        void ISerializable.GetObjectData(
+         SerializationInfo info, StreamingContext context)
+        {
+            // Instead of serializing this object, 
+            // serialize a SingletonSerializationHelp instead.
+            info.SetType(typeof(SingletonSerializationHelper));
+            // No other values need to be added.
+        }
+    }
+
+    [Serializable]
+    internal sealed class SingletonSerializationHelper : IObjectReference
+    {
+        // This object has no fields (although it could).
+
+        // GetRealObject is called after this object is deserialized.
+        public Object GetRealObject(StreamingContext context)
+        {
+            // When deserialiing this object, return a reference to 
+            // the Singleton object instead.
+            return SingleTonClass.Instance;
+        }
+    }
+}
